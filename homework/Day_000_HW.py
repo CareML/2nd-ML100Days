32094dc40985f41e049ec610dd37c60487a49c6c
Care ML <mlhuang@ntu.edu.tw>
2019-04-10 00:32:32 +0800

    Update Day_000_HW.py

--- homework/Day_000_HW.py
+++ homework/Day_000_HW.py
@@ -1,6 +1,11 @@
-# Python version 3.6.8
-# pip install requests
-# pip install BeautifulSoup4
-
+'''
+Author: CareML
+Date: 2019/4/10
+Description: Test for Day000
+req.:
+    Python version 3.6.8
+    pip install requests
+    pip install BeautifulSoup4
+'''
 import requests
 from bs4 import BeautifulSoup
